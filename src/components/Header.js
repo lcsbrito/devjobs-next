db83ec5a8a1ea00ffe58832defd2658072be3445
Lucas Brito <lcsbrito.pro@gmail.com>
2023-04-28 16:57:38 +0200

    🍀 "Post" btn added in Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,31 @@
 "use client";
-
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import styles from "../styles/header.module.css";
 
 function Header({}) {
   const [isChecked, setIsChecked] = useState(false);
+  const { push } = useRouter();
+
+  const handleCreate = () => {
+    push("/create/job");
+  };
 
   return (
     <header className={styles.header}>
       <h1 className={styles.logo}>devjobs</h1>
-      <label className={styles.switch}>
-        <input
-          type="checkbox"
-          checked={isChecked}
-          onChange={() => setIsChecked(!isChecked)}
-        />
+      <div className={styles.buttoncontainer}>
+        <button onClick={handleCreate}>Create new</button>
+        <label className={styles.switch}>
+          <input
+            type="checkbox"
+            checked={isChecked}
+            onChange={() => setIsChecked(!isChecked)}
+          />
 
-        <span></span>
-      </label>
+          <span></span>
+        </label>
+      </div>
     </header>
   );
 }
